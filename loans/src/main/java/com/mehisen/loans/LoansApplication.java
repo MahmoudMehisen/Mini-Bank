ee19f0c296494447625711c8838741f66869df3d
MahmoudMehisen <mahmoud.mehisen@gmail.com>
2024-02-24 10:54:52 +0200

    Add Config for servers

--- loans/src/main/java/com/mehisen/loans/LoansApplication.java
+++ loans/src/main/java/com/mehisen/loans/LoansApplication.java
@@ -1,24 +1,29 @@
 package com.mehisen.loans;
 
+import com.mehisen.loans.dto.LoanContactInfoDto;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @OpenAPIDefinition(
-		info = @Info(
-				title = "Cards microservice REST API Documentation",
-				description = "Mini Bank Cards microservice REST API Documentation",
-				version = "v1",
-				contact = @Contact(name = "Mahmoud Mehisen", email = "mahmoud.mehisen@gmail.com")
-		)
+        info = @Info(
+                title = "Cards microservice REST API Documentation",
+                description = "Mini Bank Cards microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(name = "Mahmoud Mehisen", email = "mahmoud.mehisen@gmail.com")
+        )
 )
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {LoanContactInfoDto.class})
 public class LoansApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoansApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LoansApplication.class, args);
+    }
 
 }
